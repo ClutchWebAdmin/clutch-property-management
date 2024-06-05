ce51e1ae0cb016e23975c44172316b173f70065d
gabewolford <gabewolford@gmail.com>
2024-06-05 14:47:16 -0700

    added properties page

---
+++ src/app/properties/page.js
@@ -0,0 +1,33 @@
+export const metadata = {
+  title: "Properties | Clutch Property Management",
+  description: "",
+  keywords: "",
+  openGraph: {
+    title: "Properties | Clutch Property Management",
+    description: "",
+    siteName: "Clutch Property Management",
+    type: "website",
+    locale: "en_US",
+    url: "https://clutchpropertymanagement.com/properties",
+    images: [
+      {
+        url: "https://clutchpropertymanagement.com/images/og-image.png",
+        alt: "Clutch Property Management",
+      },
+    ],
+  },
+  images: [
+    {
+      url: "https://clutchpropertymanagement.com/images/og-image.png",
+      alt: "Clutch Property Management",
+    },
+  ],
+};
+
+export default function ServicesPage() {
+  return (
+    <main className="pt-[103px] md:pt-[115px]">
+      <section className="h-[900px] px-5">Properties Page</section>
+    </main>
+  );
+}
