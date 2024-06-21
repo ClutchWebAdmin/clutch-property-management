b06a38698bc472bfb6e958259324a4634c977f15
Gabe Wolford <gabewolford@gmail.com>
2024-06-20 18:09:00 -0700

    dynamically generating property metadata and og tags for detail pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,38 +4,11 @@ import "./globals.css";
 import TheFooter from "@/app/components/TheFooter";
 import AOSLoader from "@/app/utils/AOSLoader";
 
-export const metadata = {
-  title: "Clutch Property Management",
-  description: "Premier properties, professionally managed.",
-  keywords:
-    "property management, property managers, salem oregon, commercial property management, residential property management",
-  openGraph: {
-    title: "Clutch Property Management",
-    description: "Premier properties, professionally managed.",
-    siteName: "Clutch Property Management",
-    type: "website",
-    locale: "en_US",
-    url: "https://clutchpropertymanagement.com",
-    images: [
-      {
-        url: "https://clutchpropertymanagement.com/images/og-image.png",
-        alt: "Clutch Property Management",
-      },
-    ],
-  },
-  images: [
-    {
-      url: "https://clutchpropertymanagement.com/images/og-image.png",
-      alt: "Clutch Property Management",
-    },
-  ],
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`min-h-svh w-screen flex flex-col mx-auto relative ${switzer.className} antialiased`}
+        className={`min-h-svh w-screen max-w-full flex flex-col mx-auto relative ${switzer.className} antialiased`}
       >
         {children}
         <TheFooter />
--- src/app/properties/[slug]/page.js
+++ src/app/properties/[slug]/page.js
@@ -1,15 +1,93 @@
 import { client } from "../../../../sanity/lib/client";
+import Image from "next/image";
 
 export async function generateStaticParams() {
   const data = await client.fetch(`
     *[_type == "properties"]{
         "slug": slug.current,
     }
-    `);
+  `);
 
   return data.map((property) => ({
     slug: property.slug,
   }));
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+
+  const data = await client.fetch(
+    `
+    *[_type == "properties" && slug.current == $slug]{
+      name,
+      type,
+      "slug": slug.current,
+      "addressLine1": address.addressLine1,
+      "addressLine2": address.addressLine2,
+      "city": address.city,
+      "state": address.state,
+      "zip": address.zip,
+      "featuredPhoto": featuredPhoto.asset->url,
+      additionalPhotos,
+      isExternallyLinked,
+      url,
+    }
+  `,
+    { slug }
+  );
+
+  const propertySlug = data[0].slug;
+  const propertyType = data[0].type;
+  const propertyName = data[0].name; // Change from data[0].siteName to data[0].name
+  const propertyLine1 = data[0].addressLine1;
+  const propertyLine2 = data[0].addressLine2;
+  const propertyCity = data[0].city;
+  const propertyState = data[0].state;
+  const propertyZip = data[0].zip;
+  const formattedProperty = propertyLine2
+    ? `${propertyLine1}, #${propertyLine2}, ${propertyCity}, ${propertyState} ${propertyZip}`
+    : `${propertyLine1}, ${propertyCity}, ${propertyState} ${propertyZip}`;
+  const isExternal = data[0].isExternallyLinked;
+  const propertyPhoto = data[0].featuredPhoto;
+  let propertyURL = data[0].url;
+
+  let metaURL;
+  if (isExternal) {
+    metaURL = propertyURL;
+  } else {
+    metaURL = `https://clutchpropertymanagement.com/properties/${propertySlug}`;
+  }
+
+  let isAnApartment;
+  propertyType === "residential"
+    ? (isAnApartment = "best cheap apartments in salem oregon")
+    : "";
+
+  return {
+    title: `${propertyLine1} | ${propertyName} | Clutch Property Management`,
+    description: `${formattedProperty}`,
+    keywords: `${propertyType} properties in salem oregon, cheap ${propertyType} properties, salem oregon, ${isAnApartment}`,
+    openGraph: {
+      title: `${propertyName} | ${propertyLine1} | Clutch Property Management`,
+      description: `${formattedProperty}`,
+      siteName: "Clutch Property Management",
+      type: "website",
+      locale: "en_US",
+      url: `${metaURL}`,
+      images: [
+        {
+          url: `${propertyPhoto}`,
+          alt: `${propertyName}`,
+        },
+      ],
+    },
+    images: [
+      {
+        url: `${propertyPhoto}`,
+        alt: `${propertyName}`,
+      },
+    ],
+  };
 }
 
 export default async function PropertyDetailPage({ params }) {
@@ -18,19 +96,23 @@ export default async function PropertyDetailPage({ params }) {
   const data = await client.fetch(
     `
     *[_type == "properties" && slug.current == $slug]{
-      _createdAt,
-      _updatedAt,
-      _id,
       name,
       available,
       price,
       sqFootage,
+      bedrooms,
+      bathrooms,
       "slug": slug.current,
       "addressLine1": address.addressLine1,
       "addressLine2": address.addressLine2,
       "city": address.city,
       "state": address.state,
       "zip": address.zip,
+      "imageUrl": featuredPhoto.asset->url,
+      "altText": featuredPhoto.alt,
+      "height": featuredPhoto.asset->metadata.dimensions.height,
+      "width": featuredPhoto.asset->metadata.dimensions.width,
+      "blurDataURL": featuredPhoto.asset->metadata.lqip,
       additionalPhotos,
       isExternallyLinked,
       url,
@@ -41,20 +123,38 @@ export default async function PropertyDetailPage({ params }) {
 
   const property = data[0];
 
+  const formattedAddress = property.addressLine2 ? (
+    <div className="flex flex-col">
+      <p className="text-xl">
+        {`${property.addressLine1} #${property.addressLine2}`} <br />
+        {property.city}, {property.state} {property.zip}
+      </p>
+    </div>
+  ) : (
+    <p className="text-xl">
+      {`${property.addressLine1}`} <br />
+      {property.city}, {property.state} {property.zip}
+    </p>
+  );
+
   return (
     <main>
       <section className="pt-[var(--header-height)] border-b border-secondaryBlue">
-        <div className="h-[600px] p-5">
-          <h2>Property Detail Page</h2>
-          <p>ID: {property._id}</p>
-          <p>Slug: {property.slug}</p>
-          <p>Address Line 1: {property.addressLine1}</p>
-          <p>Address Line 2: {property.addressLine2}</p>
-          <p>City: {property.city}</p>
-          <p>State: {property.state}</p>
-          <p>Zip: {property.zip}</p>
-          <p>Price: {property.price}</p>
-          <p>Sq Footage: {property.sqFootage}</p>
+        <div className="flex flex-col lg:flex-row h-[600px]">
+          <div className="flex flex-col w-full lg:w-1/2 py-10 px-5">
+            <h3 className="text-3xl font-medium">{property.name}</h3>
+            {formattedAddress}
+          </div>
+
+          <Image
+            src={property.imageUrl}
+            className="w-full lg:w-1/2 h-auto object-cover py-10 px-5"
+            alt={property.altText}
+            placeholder="blur"
+            blurDataURL={property.blurDataURL}
+            height={property.height}
+            width={property.width}
+          />
         </div>
       </section>
     </main>
