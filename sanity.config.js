4bd0a8061fc9f2f332b8ce7395e3c892916704e2
Gabe Wolford <gabewolford@gmail.com>
2024-07-16 11:35:55 -0700

    install vercel deploy tool, and setup webhooks, tokens, etc

--- sanity.config.js
+++ sanity.config.js
@@ -1,27 +1,29 @@
-'use client'
+"use client";
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...tool]]/page.jsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from "@sanity/vision";
+import { defineConfig, isDev } from "sanity";
+import { structureTool } from "sanity/structure";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
 
 export default defineConfig({
-  basePath: '/admin',
+  basePath: "/admin",
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-})
+  plugins: isDev
+    ? [
+        structureTool(),
+        visionTool({ defaultApiVersion: apiVersion }),
+        vercelDeployTool(),
+      ]
+    : [structureTool(), vercelDeployTool()],
+});
