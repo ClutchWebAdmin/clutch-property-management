cdcf2df113288545efe58e3d3e537b04761eda0b
Gabe Wolford <gabewolford@gmail.com>
2024-07-22 18:00:14 -0700

    configured gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import TheFooter from "@/app/components/TheFooter";
 import AOSLoader from "@/app/utils/AOSLoader";
 import { Analytics } from "@vercel/analytics/react";
 import CookieModal from "./components/CookieModal";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
@@ -17,6 +18,8 @@ export default function RootLayout({ children }) {
         <AOSLoader />
         <Analytics />
         <CookieModal />
+        <GoogleTagManager gtmId="GTM-5K7QF5K4" />
+        <GoogleTagManager gtmId="GTM-MHH3MTZ3" />
       </body>
     </html>
   );
