51d73765def20c1ccdefae63ac94320c9d90b89c
Gabe Wolford <gabewolford@gmail.com>
2024-07-23 11:26:31 -0700

    updated hero copy to include salem oregon

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function HomePage() {
       <TheHeader variant="light" />
       <main>
         <Hero
-          subheading={`We are your experts in seamless property operations, providing dependable service and care for all of our properties.`}
+          subheading={`We are your Salem, Oregon experts in seamless property operations, providing dependable service and care for all of our properties.`}
         />
         <HeadingSection variant={`white`} text={`About Us`} />
         <TwoColumnSection
