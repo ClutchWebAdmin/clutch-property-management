a4562a6aeb4c135aa896f24208e8221dcbdc16f2
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-11-27 16:26:31 -0800

    Updating availability of properties

--- src/app/components/UI/ContactButton.jsx
+++ src/app/components/UI/ContactButton.jsx
@@ -5,3 +5,5 @@ import { PopupButton } from "@typeform/embed-react";
 export default function ContactButton({ buttonText }) {
   return <PopupButton id="H0kgpUHf">{buttonText}</PopupButton>;
 }
+
+{/* <PopupButton id="SzoxRJkL">{buttonText}</PopupButton> */}
